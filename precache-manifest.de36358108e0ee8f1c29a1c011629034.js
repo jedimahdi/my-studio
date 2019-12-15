585e06992ecb75ff0dec12515943300df171b680
jedimahdi <seyedmahdiseydan78@yahoo.com>
2019-12-15 22:17:53 +0330

    Updates

---
+++ precache-manifest.de36358108e0ee8f1c29a1c011629034.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7987fbc0eaaefbc93f41ed7ea3ce7d4c",
+    "url": "/my-studio/index.html"
+  },
+  {
+    "revision": "6028dcb7935ffa430718",
+    "url": "/my-studio/static/css/main.e971b92c.chunk.css"
+  },
+  {
+    "revision": "8b4675ffa6512fab7273",
+    "url": "/my-studio/static/js/2.912ff963.chunk.js"
+  },
+  {
+    "revision": "6028dcb7935ffa430718",
+    "url": "/my-studio/static/js/main.a9e1c227.chunk.js"
+  },
+  {
+    "revision": "074cdef9738e5a2b13bd",
+    "url": "/my-studio/static/js/runtime~main.a56b931b.js"
+  }
+]);
